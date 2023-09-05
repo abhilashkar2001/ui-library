f38dd0443c0b8785e86e99aa5c4073513a6965b6
Bibhuti-Mahapatra <bibhuti.mahapatra@rumango.com>
2023-09-05 16:43:10 +0530

    changes

--- src/app/shared/components/success-popup/success-popup.component.ts
+++ src/app/shared/components/success-popup/success-popup.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-success-popup",
@@ -7,14 +8,19 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
   styleUrls: ["./success-popup.component.scss"],
 })
 export class SuccessPopupComponent implements OnInit {
+  originationId: any;
   constructor(
     private dialogRef: MatDialogRef<SuccessPopupComponent>,
-    @Inject(MAT_DIALOG_DATA) private data: any
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.originationId = this.data.originationId;
+  }
   done() {
     this.dialogRef.close();
-    window.close();
+    this.router.navigate(["account/landing"]);
+    //window.close();
   }
 }
