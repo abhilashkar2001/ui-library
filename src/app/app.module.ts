267ec83155745b6e711adad967785e23fdb674f0
sakthivelrumango <sakthivel.m@rumango.com>
2023-09-06 11:06:33 +0530

    swiper added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ErrorHandlerService } from "./shared/services/error-handler.service";
 import { TokenInterceptor } from "./shared/interceptors/token.interceptor";
 import { LayoutsModule } from "./layouts/layouts.module";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-//import { SwiperModule } from "swiper/angular";
+import { SwiperModule } from 'swiper/angular';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
@@ -42,7 +42,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     LayoutsModule,
     SharedModule,
     HttpClientModule,
-    //SwiperModule,
+    SwiperModule,
     PerfectScrollbarModule,
     TranslateModule.forRoot({
       loader: {
