afb18e1789633714712075eda960d42796c9b956
Rupali851 <104140670+Rupali851@users.noreply.github.com>
2023-09-07 18:54:58 +0530

    application number dyanamic

--- src/app/modules/new-deposit/new-deposit/fd-calculator/success-popup/success-popup.component.ts
+++ src/app/modules/new-deposit/new-deposit/fd-calculator/success-popup/success-popup.component.ts
@@ -7,12 +7,15 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
   styleUrls: ["./success-popup.component.scss"],
 })
 export class SuccessPopupComponent implements OnInit {
+  depositId: any;
   constructor(
     private dialogRef: MatDialogRef<SuccessPopupComponent>,
     @Inject(MAT_DIALOG_DATA) private data: any
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.depositId = parseInt(sessionStorage.getItem("recurringDepositId"));
+  }
   done() {
     this.dialogRef.close();
     window.close();
