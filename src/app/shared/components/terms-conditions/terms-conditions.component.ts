61c39de61b8861a5219fb62545b10706febdc806
SaswatiBehera <104128993+SaswatiBehera@users.noreply.github.com>
2023-09-07 21:43:12 +0530

    terms and condition page done

--- src/app/shared/components/terms-conditions/terms-conditions.component.ts
+++ src/app/shared/components/terms-conditions/terms-conditions.component.ts
@@ -1,5 +1,13 @@
 import { Location } from "@angular/common";
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
 
 @Component({
   selector: "app-terms-conditions",
@@ -10,10 +18,18 @@ export class TermsConditionsComponent implements OnInit {
   @Output() onConfirmEvent: EventEmitter<any> = new EventEmitter();
   @Output() onBackEvent: EventEmitter<any> = new EventEmitter();
   checked = false;
+  customerName: any;
+  customerData: any;
+  requestDate: any;
 
   constructor(private _location: Location) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.customerData = JSON.parse(localStorage.getItem("customerData"));
+    this.customerName =
+      this.customerData?.firstName + " " + this.customerData?.lastName;
+    this.requestDate = this.customerData?.requestDate.replace(/[a-zA-Z]/g, " ");
+  }
 
   isValidated() {
     if (!this.checked) {
