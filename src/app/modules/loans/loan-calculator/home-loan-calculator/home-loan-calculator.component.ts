8ad9109a2a88329e713fa97fa822bd3b882cd2ee
sagar suryawamshi <sagar.dilip@rumango.com>
2023-09-25 18:40:35 +0530

    changes

--- src/app/modules/loans/loan-calculator/home-loan-calculator/home-loan-calculator.component.ts
+++ src/app/modules/loans/loan-calculator/home-loan-calculator/home-loan-calculator.component.ts
@@ -71,14 +71,7 @@ export class HomeLoanCalculatorComponent implements OnInit {
       outstandingAmount: 0,
     });
   }
-  updateDeposit() {
-    console.log(this.loanForm.value);
-  }
   applyForLoan() {
-    console.log(this.loanForm.value);
-    sessionStorage.setItem("tenureDays", this.loanForm.value.tenureDays);
-    sessionStorage.setItem("tenureYear", this.loanForm.value.tenureYear);
-    sessionStorage.setItem("tenureMonth", this.loanForm.value.tenureMonth);
     this.customCalculatorValues.emit(this.loanForm.value);
   }
 }
