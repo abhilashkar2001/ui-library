7acc8c67b17037d1f7e538ee5e2edf3cbb36e299
unknown <sagar.dilip@rumango.com>
2024-03-07 16:04:03 +0530

    trade flow base

---
+++ src/app/modules/net-banking/trade-flow/trade-dashboard/trade-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trade-dashboard',
+  templateUrl: './trade-dashboard.component.html',
+  styleUrls: ['./trade-dashboard.component.scss']
+})
+export class TradeDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
