e5483b26ddda05e8d3447e73a38548af305bc413
unknown <sagar.dilip@rumango.com>
2024-03-22 15:41:32 +0530

    constant file added

---
+++ src/app/modules/loans/create-loan/create-loan.constant.ts
@@ -0,0 +1,24 @@
+export class CreateLoanConstant {
+  static readonly CURRENCY_SYMBOLL = "₹";
+  static readonly SCREEN_NAME = "Create Loan";
+  static readonly GENERIC_SATIC_KEYS = {
+    HOLDERTYPE: [],
+    DISBURSEMENTTYPE: [],
+  };
+
+  static readonly ACCOUNT_TYPE = [
+    {
+      name: "Internal Account",
+      value: "internal",
+    },
+    {
+      name: "External Account",
+      value: "external",
+    },
+  ];
+}
+export enum CreateLoanEnum {
+  INTERNAL = "internal",
+  EXTERNAL = "external",
+  ACCOUNT_INCLUDES_KEY = "account",
+}
