3ec56475e6568c856fc7732a1b0b56fddd557d8d
manisha0098 <118351379+manisha0098@users.noreply.github.com>
2024-03-28 19:01:23 +0530

    good-info screen done

--- src/app/modules/net-banking/net-banking-routing.module.ts
+++ src/app/modules/net-banking/net-banking-routing.module.ts
@@ -18,6 +18,7 @@ import { GenericBgComponentComponent } from "./new-reusable-components/generic-b
 import { BgInfoComponent } from "./new-reusable-components/bg-info/bg-info.component";
 import { BenificiaryBulkUploadSummaryComponent } from "./trade-flow/beneficiary-summary/benificiary-bulk-upload-summary/benificiary-bulk-upload-summary.component";
 import { BgSummaryComponent } from "./trade-flow/bg-summary/bg-summary.component";
+import { GoodsInfoComponent } from "./new-reusable-components/goods-info/goods-info.component";
 
 const routes: Routes = [
   {
@@ -45,6 +46,10 @@ const routes: Routes = [
           {
             path: "dashboard",
             component: TradeDashboardComponent,
+          },
+          {
+            path: "good-info",
+            component: GoodsInfoComponent,
           },
 
           {
@@ -97,4 +102,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class NetBankingRoutingModule {}
+export class NetBankingRoutingModule { }
