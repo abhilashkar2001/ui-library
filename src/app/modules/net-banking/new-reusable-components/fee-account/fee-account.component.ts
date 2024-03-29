b82e7fdad6f5b1e782cdc0a4010388472069beb3
manisha0098 <118351379+manisha0098@users.noreply.github.com>
2024-03-29 17:50:38 +0530

    fee amount structure done

--- src/app/modules/net-banking/new-reusable-components/fee-account/fee-account.component.ts
+++ src/app/modules/net-banking/new-reusable-components/fee-account/fee-account.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-fee-account',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fee-account.component.scss']
 })
 export class FeeAccountComponent implements OnInit {
-
-  constructor() { }
+  @Input('feeAccArray') feeAccArray: any = [];
+  feeAccountForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    console.log(this.feeAccArray);
+    this.buildInfoForm();
+  }
+
+
+  buildInfoForm() {
+    this.feeAccountForm = this.formBuilder.group({
+      feeAccountModel: this.formBuilder.array([]),
+    });
+  }
+
+  public get feeAccountModel(): FormArray {
+    return this.feeAccountForm?.get("feeAccountModel") as FormArray;
+  }
+
+  addTitleCategory(index: number, accountName: string): void {
+    this.feeAccountModel.push(this.formBuilder.control(accountName));
+    this.feeAccArray.splice(index, 1);
+  }
+
+  removeTitleCategoty(index: number, accountName: string): void {
+    this.feeAccountModel.removeAt(index);
+    this.feeAccArray.push(accountName);
   }
 
 }
