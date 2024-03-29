de456f4445415eaeb0cd8c2c50ba8ef143c93fa5
Bibhuti-Mahapatra <bibhuti.mahapatra@rumango.com>
2024-03-30 00:56:21 +0530

    Amendment Lc info and reusable buttons

--- src/app/modules/net-banking/net-banking.module.ts
+++ src/app/modules/net-banking/net-banking.module.ts
@@ -39,6 +39,7 @@ import { LcInfoComponent } from './new-reusable-components/lc-info/lc-info.compo
 import { GoodsInfoComponent } from './new-reusable-components/goods-info/goods-info.component';
 import { LcAdditionalInfoComponent } from './new-reusable-components/lc-additional-info/lc-additional-info.component';
 import { FeeAccountComponent } from './new-reusable-components/fee-account/fee-account.component';
+import { AmendmentLcInfoComponent } from './new-reusable-components/amendment-lc-info/amendment-lc-info.component';
 
 
 @NgModule({
@@ -73,6 +74,7 @@ import { FeeAccountComponent } from './new-reusable-components/fee-account/fee-a
     GoodsInfoComponent,
     LcAdditionalInfoComponent,
     FeeAccountComponent,
+    AmendmentLcInfoComponent,
   ],
   imports: [
     CommonModule,
