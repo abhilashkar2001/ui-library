f463cfb96305f20d5bc5f7cde303d1d0aae0a67d
Bibhuti-Mahapatra <bibhuti.mahapatra@rumango.com>
2024-05-03 17:42:58 +0530

    Multi and single full flow

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -84,6 +84,7 @@ import { IcCustomInputComponent } from "./ic-custom-input/ic-custom-input.compon
 import { IcRowInputComponent } from "./ic-row-input/ic-row-input.component";
 import { ReusableTableComponent } from "./reusable-table/reusable-table.component";
 import { SelectSingleTransferComponent } from "./select-single-transfer/select-single-transfer.component";
+import { CustomSuccessPopupComponent } from "./custom-success-popup/custom-success-popup.component";
 
 // Pass the fusioncharts library and chart modules
 FusionChartsModule.fcRoot(FusionCharts, Charts, Widgets, FusionTheme);
@@ -142,6 +143,7 @@ const components = [
   IcCustomInputComponent,
   IcRowInputComponent,
   SelectSingleTransferComponent,
+  CustomSuccessPopupComponent,
 ];
 
 @NgModule({
