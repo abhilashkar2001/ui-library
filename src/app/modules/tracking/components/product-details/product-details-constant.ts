224d0f76fc0c5d0b19e334f15d8d5939aa86677f
sagar suryawanshi <93503940+sagarsd1@users.noreply.github.com>
2024-05-13 18:41:36 +0530

    Update product-details-constant.ts

--- src/app/modules/tracking/components/product-details/product-details-constant.ts
+++ src/app/modules/tracking/components/product-details/product-details-constant.ts
@@ -1,7 +1,6 @@
 export class ProductConstant {
   static LoanDynamicKeys = {
-    // Define keys for loan products
-    // For example:
+
     loanAccountInfo: "Loan Account Information",
     bankAccount: "Bank Account",
     disbursementDetails: "Disbursement Details",
@@ -11,8 +10,6 @@ export class ProductConstant {
   };
 
   static AccountDynamicKeys = {
-    // Define keys for non-loan products
-    // For example:
     customerInfo: "Customer Information",
     docs: "KYC Documents",
   };
