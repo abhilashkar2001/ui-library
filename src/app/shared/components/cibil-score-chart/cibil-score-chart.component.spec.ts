fa399130507538f08eb17d064163dfa244ff5cfd
Bhagabati Prasad <prasad.b@rumango.com>
2024-05-20 18:22:26 +0530

    created a credit score chart

---
+++ src/app/shared/components/cibil-score-chart/cibil-score-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CibilScoreChartComponent } from './cibil-score-chart.component';
+
+describe('CibilScoreChartComponent', () => {
+  let component: CibilScoreChartComponent;
+  let fixture: ComponentFixture<CibilScoreChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CibilScoreChartComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CibilScoreChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
