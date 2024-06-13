5248953c746fdec3766490b9848d8b97df3d75e6
shiyam-rumango <shiyam.ram@rumango.com>
2024-06-13 22:24:14 +0530

    Changes email body changes

--- src/app/modules/create-account/create-account/create-account-landing-page/create-account-landing-page.component.ts
+++ src/app/modules/create-account/create-account/create-account-landing-page/create-account-landing-page.component.ts
@@ -491,12 +491,12 @@ export class CreateAccountLandingPageComponent {
     const formData: FormData = new FormData();
     formData.append(
       "subject",
-      "Thank you for submitting your loan application through our website."
+      "Thank you for submitting your application through our website."
     );
     formData.append(
       "body",
       `Dear ${applicantName},\n
-Thank you for submitting your loan application through our website.
+Thank you for submitting your application through our website.
 
 
 We are pleased to inform you that your application has been successfully received and forwarded to the bank.\n
