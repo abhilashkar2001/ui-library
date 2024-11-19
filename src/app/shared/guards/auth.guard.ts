9e09048f208d63c184cb250838f766cfcfc87efc
Abhilash Kar <102718141+abhilashkar2001@users.noreply.github.com>
2024-11-19 17:36:56 +0530

    CLI and Core upgraded to version 16

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,14 +1,9 @@
 import { Injectable } from "@angular/core";
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-} from "@angular/router";
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 import { TokenStorageService } from "../token-storage.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     private router: Router,
     private tokenService: TokenStorageService
