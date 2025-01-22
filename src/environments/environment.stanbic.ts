9c7f5ec1194cc119a0ee5c0c84de3d9ced1bb470
Abhilash Kar <102718141+abhilashkar2001@users.noreply.github.com>
2025-01-22 18:50:48 +0530

    Update environment.stanbic.ts

--- src/environments/environment.stanbic.ts
+++ src/environments/environment.stanbic.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  microServiceURL: 'https://172.16.1.124:8767',
+  microServiceURL: 'https://192.168.131.27:8765',
   build: 32,
     SECRET_KEY:
     'e1cd356f6de53babfef423718f055d8034d5d580c4196f6c5f230e2ee395133b',
