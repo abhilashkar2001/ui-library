89ba22da964fbb8d474af20614bd20bcf7c07261
rahulvarmass8 <rahul.varma@rumango.com>
2025-01-28 19:44:59 +0530

    feat:value inside object model; fix: branch to branchId, but branchCode is needed

--- src/app/modules/loans/loan-flow/loan-flow.component.ts
+++ src/app/modules/loans/loan-flow/loan-flow.component.ts
@@ -256,7 +256,7 @@ export class LoanFlowComponent implements OnInit, OnDestroy {
       ...this.disbursementDetails,
       disbursementType: data.disbursementType,
       bankCode: store?.bankCode,
-      branchCode: store?.branch,
+      branchCode: store?.branchId,
       originationId: this.sessionStorageService.getOriginationId(),
     };
 
@@ -277,7 +277,7 @@ export class LoanFlowComponent implements OnInit, OnDestroy {
     payload.disbursementAccInfo = {
       accountNo: data.accountNumber,
       bankCode: store?.bankCode,
-      branchCode: store?.branch,
+      branchCode: store?.branchId,
     };
     return payload;
   }
@@ -473,7 +473,7 @@ export class LoanFlowComponent implements OnInit, OnDestroy {
         loanTenureDay: this.sessionStorageService.getTenureDays(),
         loanTenureMonth: this.sessionStorageService.getTenureMonth(),
         loanTenureYear: this.sessionStorageService.getTenureYear(),
-        branchCode: this.currentUser?.branch,
+        branchCode: this.currentUser?.branchId,
         source: 'Website',
         businessProductName: this.productDetails.basisName,
         productDescription: this.productDetails.basisDetailStory,
@@ -575,7 +575,7 @@ export class LoanFlowComponent implements OnInit, OnDestroy {
       loanTenureDay: this.sessionStorageService.getTenureDays(),
       loanTenureMonth: this.sessionStorageService.getTenureMonth(),
       loanTenureYear: this.sessionStorageService.getTenureYear(),
-      branchCode: this.currentUser?.branch,
+      branchCode: this.currentUser?.branchId,
       source: 'Website',
       businessProductName: null,
       productDescription: null,
--- src/app/modules/new-deposit/new-deposit/fd-calculator/fixed-deposit-details/fixed-deposit-details.component.ts
+++ src/app/modules/new-deposit/new-deposit/fd-calculator/fixed-deposit-details/fixed-deposit-details.component.ts
@@ -96,7 +96,7 @@ export class FixedDepositDetailsComponent implements OnInit, OnDestroy {
       if (result) {
         this.currentUser = result;
         if (this.currentUser) {
-          this.currentUserBranch = this.currentUser?.branch;
+          this.currentUserBranch = this.currentUser?.branchId;
         }
       }
     });
