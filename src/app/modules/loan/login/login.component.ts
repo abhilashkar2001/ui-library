91dd07da1b2666daba8c71857333d7f4c732446b
shiyam-rumango <139947132+shiyam-rumango@users.noreply.github.com>
2025-06-18 04:28:33 +0530

    Loginmobile (#770)
    * shiyam-15
    
    * fix:fix
    
    * fix
    
    * fix: new login changes

--- src/app/modules/loan/login/login.component.ts
+++ src/app/modules/loan/login/login.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { CommonService } from 'app/shared/services/common-service/common.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'login',
+  selector: 'app-login',
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  countriesIsdCodes: any[] = [];
+  countryTelIsdCode: any;
+  defaultIsdCodeValue: any;
+  maxMobileLength: any;
+  subscriptions: Subscription[] = [];
+  otpForm!: FormGroup;
+
+  constructor(
+    private commonService: CommonService,
+    private fb: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.loadCountries();
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.otpForm = this.fb.group({
+      phone: [''],
+      isdCode: [''],
+    });
+  }
+  // Get All Countrys and Isd code Mthd
+  loadCountries() {
+    this.commonService.getAllCountries().subscribe((resp: any) => {
+      if (resp.data.length > 0) {
+        this.countriesIsdCodes = resp?.data?.map(
+          (country: any) => country.countryTelIsdCode,
+        );
+      } else {
+        this.countriesIsdCodes = [];
+      }
+    });
+  }
+}
