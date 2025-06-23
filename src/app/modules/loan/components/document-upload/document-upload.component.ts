472b36b99ab40136c97cc2396c5cddc095932813
shiyam-rumango <139947132+shiyam-rumango@users.noreply.github.com>
2025-06-23 22:16:57 +0530

    Document (#821)
    * shiyam-15
    
    * fix:fix
    
    * fix
    
    * document-upload

--- src/app/modules/loan/components/document-upload/document-upload.component.ts
+++ src/app/modules/loan/components/document-upload/document-upload.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoanService } from 'app/shared/services/loan/loan.service';
+import { SessionStorageService } from 'app/shared/services/session-storage.service';
 
 @Component({
   selector: 'app-document-upload',
   templateUrl: './document-upload.component.html',
   styleUrls: ['./document-upload.component.scss'],
 })
-export class DocumentUploadComponent {}
+export class DocumentUploadComponent implements OnInit {
+  checkListDocList: any[] = [];
+
+  constructor(
+    private loanApi: LoanService,
+    private sessionStorageService: SessionStorageService,
+  ) {}
+
+  ngOnInit() {
+    this.loanApi
+      .getCheckListDocNoOrigination(
+        this.sessionStorageService.getCurrentStage() ?? 131706,
+        parseInt(this.sessionStorageService.getCurrentScreenCode() ?? 456),
+      )
+      .subscribe((resp) => {
+        if (resp.data.length > 0) {
+          this.checkListDocList = this.groupBy(resp?.data);
+        } else {
+          this.checkListDocList = [];
+        }
+      });
+  }
+
+  groupBy(documents: any) {
+    return documents.reduce((result: any, doc: any) => {
+      const groupName = doc.docRequired
+        ? 'requiredDocument'
+        : 'nonRequiredDocument';
+      (result[groupName] = result[groupName] || []).push(doc);
+      return result;
+    }, {});
+  }
+}
