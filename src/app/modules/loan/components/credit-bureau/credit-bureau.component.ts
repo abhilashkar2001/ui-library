ca0dced23658f2e8c65eb4b7e76898c74de86771
sanjanarumango <134698152+sanjanarumango@users.noreply.github.com>
2025-07-07 10:53:43 +0530

    feat: fix design and integration (#880)

--- src/app/modules/loan/components/credit-bureau/credit-bureau.component.ts
+++ src/app/modules/loan/components/credit-bureau/credit-bureau.component.ts
@@ -1,6 +1,8 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { TrackingService } from 'app/modules/origination/modules/tracking/tracking-service';
+import { LoanService } from 'app/shared/services/loan/loan.service';
 import { of } from 'rxjs';
 
 @Component({
@@ -8,13 +10,27 @@ import { of } from 'rxjs';
   templateUrl: './credit-bureau.component.html',
   styleUrls: ['./credit-bureau.component.scss'],
 })
-export class CreditBureauComponent {
+export class CreditBureauComponent implements OnInit {
   otp = new FormControl('', [Validators.required]);
-  showOtpSection = false;
+  showOtpSection = true;
 
   @ViewChild('consentDialog') consentDialog!: TemplateRef<any>;
+  creditBureau: any;
+  otpSent: boolean | undefined;
+  resendLink: boolean | undefined;
+  intervalId: any;
+  displaySecond: string | any;
+  invalidOtp = false;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private loanService: LoanService,
+    private otpService: TrackingService,
+  ) {}
+
+  ngOnInit() {
+    this.fetchCreditBureau();
+  }
 
   openDialog() {
     this.dialog.open(this.consentDialog, {
@@ -22,6 +38,76 @@ export class CreditBureauComponent {
       height: '40%',
       panelClass: 'custom-dialog',
     });
+  }
+
+  fetchCreditBureau() {
+    this.loanService.fetchCreditBureau(962).subscribe((res: any) => {
+      if (res?.statusCode == 200 || res?.statusCode == 201) {
+        this.creditBureau = res?.data;
+        this.getOtp();
+      }
+    });
+  }
+
+  getOtp() {
+    this.otpService
+      .getOtp({ mobile: this.creditBureau?.mobile })
+      .subscribe((resp) => {
+        const otp = resp?.OTP;
+        if (otp && otp !== 'null') {
+          this.otpSent = true;
+          this.resendLink = false;
+          this.stopInterval();
+          this.otpTimer();
+        }
+      });
+  }
+
+  otpTimer() {
+    this.stopInterval();
+    const minute = 0.5;
+    let seconds: number = minute * 60;
+    let textSec: any = '0';
+    let statSec = 30;
+    const prefix = minute < 10 ? '0' : '';
+    this.intervalId = setInterval(() => {
+      seconds--;
+      if (statSec != 0) statSec--;
+      else statSec = 30;
+
+      if (statSec < 10) {
+        textSec = '0' + statSec;
+      } else textSec = statSec;
+
+      this.displaySecond = `${prefix}${Math.floor(seconds / 60)}:${textSec}`;
+      if (seconds == 0) {
+        this.resendLink = true;
+        this.stopInterval();
+      }
+    }, 1000);
+  }
+
+  /**
+   * TO clear the timer interval.
+   */
+  stopInterval() {
+    clearInterval(this.intervalId);
+  }
+
+  verifyOtp() {
+    this.otpService
+      .verifyOtp({
+        mobile: this.creditBureau?.mobile,
+        otp: this.otp,
+      })
+      .subscribe((response: any) => {
+        if (response.status === 401) {
+          this.invalidOtp = true;
+          this.showOtpSection = true;
+        } else if (response.status === 200) {
+          this.showOtpSection = false;
+        }
+      });
   }
 
   handleSubmit() {
