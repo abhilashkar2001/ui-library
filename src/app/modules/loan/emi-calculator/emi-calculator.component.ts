5007f7727b0feb28d36f05e40be4cda7d3748693
Abhilash Kar <102718141+abhilashkar2001@users.noreply.github.com>
2025-07-10 16:57:13 +0530

    feat: emi calculator

--- src/app/modules/loan/emi-calculator/emi-calculator.component.ts
+++ src/app/modules/loan/emi-calculator/emi-calculator.component.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
+import { ICalculatorReqeust } from '../../../shared/models/loan/calculator.model';
+import { LoanService } from '../../../shared/services/loan/loan.service';
+import { Location } from '@angular/common';
+import { ProductState } from '../../../shared/models/router-state.model';
 
 @Component({
   selector: 'app-emi-calculator',
   templateUrl: './emi-calculator.component.html',
   styleUrls: ['./emi-calculator.component.scss'],
 })
-export class EmiCalculatorComponent {}
+export class EmiCalculatorComponent implements OnInit {
+  calculatorForm:
+    | FormGroup<{
+        [K in keyof ICalculatorReqeust]: FormControl<ICalculatorReqeust[K]>;
+      }>
+    | undefined;
+
+  private productId: number;
+
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private loanService: LoanService,
+    private _location: Location,
+  ) {
+    this.productId = (<ProductState>this._location.getState()).productId;
+    this.initCalculatorForm();
+  }
+
+  ngOnInit() {
+    this.getProductDetails();
+  }
+
+  initCalculatorForm() {
+    this.calculatorForm = this.fb.group({
+      principleAmount: [0, Validators.required],
+      interestRate: [0, Validators.required],
+      numberOfMonths: [0, Validators.required],
+      firstRepaymentDate: ['', Validators.required],
+    });
+  }
+
+  getProductDetails() {
+    this.loanService
+      .getProductAspectDetails(this.productId)
+      .subscribe((resp) => {
+        if (resp?.statusCode === 200) {
+        }
+      });
+  }
+}
