1fe3519da0382a7c6445976dc33b46cf29f0566f
manisha0098 <118351379+manisha0098@users.noreply.github.com>
2025-07-28 18:56:03 +0530

    account selection screen done

--- src/app/modules/create-account/components/account-selection/account-selection.component.ts
+++ src/app/modules/create-account/components/account-selection/account-selection.component.ts
@@ -9,6 +9,30 @@ import { SessionStorageService } from 'app/shared/services/session-storage.servi
   styleUrls: ['./account-selection.component.scss'],
 })
 export class AccountSelectionComponent {
+  accountTypes = [
+    {
+      key: 'individual',
+      title: 'Individual Account',
+      description:
+        'An account exclusively for one person, giving you full control.',
+      img: 'individual2.svg',
+    },
+    {
+      key: 'joint',
+      title: 'Joint Account',
+      description:
+        'An account exclusively for one person, giving you full control to manage.',
+      img: 'joint2.svg',
+    },
+    {
+      key: 'minor',
+      title: 'Minor Account',
+      description:
+        'An account exclusively for one person, giving you full control to manage.',
+      img: 'minor2.svg',
+    },
+  ];
+
   constructor(
     private dialogRef: MatDialogRef<AccountSelectionComponent>,
     @Inject(MAT_DIALOG_DATA) private data: any,
