cfc29e852665729933fe7d328e1f16f01b0a7ecc
Harish01102002 <harish.sm@rumango.com>
2025-08-11 12:26:18 +0530

    routing and modules imported

--- src/app/modules/cheque-book/cheque-book.module.ts
+++ src/app/modules/cheque-book/cheque-book.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChequeBookRoutingModule } from './cheque-book-routing.module';
@@ -7,7 +7,16 @@ import { ChequeBookStagesComponent } from './cheque-book-stages/cheque-book-stag
 import { AccountServiceComponent } from './components/account-service/account-service.component';
 import { ChequeBookDetailsComponent } from './components/cheque-book-details/cheque-book-details.component';
 import { PaymentDetailsComponent } from './components/payment-details/payment-details.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
+import { IcustLibraryModule } from '@onerumango/icust-element-library';
+import { SharedComponentsModule } from 'app/shared/components/shared-components.module';
+import { SharedPipesModule } from 'app/shared/pipes/shared-pipes.module';
+import { SharedMaterialModule } from 'app/shared/shared-material.module';
+import { SharedModule } from 'app/shared/shared.module';
+import { DynamicPagesModule } from '../origination/modules/dynamic-pages/dynamic-pages.module';
+import { OriginationExternalCallbackModule } from '../origination/modules/origination-external-callback/origination-external-callback.module';
+import { SharedOriginationModule } from '../origination/modules/shared-origination/shared-origination.module';
 
 @NgModule({
   declarations: [
@@ -15,11 +24,23 @@ import { PaymentDetailsComponent } from './components/payment-details/payment-de
     ChequeBookStagesComponent,
     AccountServiceComponent,
     ChequeBookDetailsComponent,
-    PaymentDetailsComponent
+    PaymentDetailsComponent,
   ],
   imports: [
     CommonModule,
-    ChequeBookRoutingModule
-  ]
+    ChequeBookRoutingModule,
+    SharedMaterialModule,
+    SharedComponentsModule,
+    SharedModule,
+    FlexLayoutModule,
+    IcustLibraryModule,
+    SharedPipesModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedOriginationModule,
+    OriginationExternalCallbackModule,
+    DynamicPagesModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ChequeBookModule { }
+export class ChequeBookModule {}
