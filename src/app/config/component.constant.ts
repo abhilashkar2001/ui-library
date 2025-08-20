207ea2a40a7500987dcf5a854ccfce3034bb7007
ShivaDhr <shivadharshan.s@rumango.com>
2025-08-20 18:40:52 +0530

    cheque corporate field verfication

--- src/app/config/component.constant.ts
+++ src/app/config/component.constant.ts
@@ -67,7 +67,7 @@ export interface ComponentStagesMap {
   Signature: AccountDigitalSignatureComponent;
   'Business Details': BusinessDetailsComponent;
   'Director Document Upload': DocumentUploadComponent;
-  'Director Details': AccountPersonalDetailsComponent;
+  'Director Details': DirectorDetailsComponent;
   'Employement Financial Details': EmpFinDetailsComponent;
   'Terms And Conditions': TermsConditionsComponent;
   'Account Services': AccountServiceComponent;
@@ -87,7 +87,7 @@ export const ComponentStagesConstant: {
   Signature: AccountDigitalSignatureComponent,
   'Business Details': BusinessDetailsComponent,
   'Director Document Upload': DocumentUploadComponent,
-  'Director Details': AccountPersonalDetailsComponent,
+  'Director Details': DirectorDetailsComponent,
   'Employement Financial Details': EmpFinDetailsComponent,
   'Terms And Conditions': TermsConditionsComponent,
   'Account Services': AccountServiceComponent,
