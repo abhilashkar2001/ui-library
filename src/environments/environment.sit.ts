98b8bf76397bdabbc6e61820c8c8c00e1c007760
Abhilash Kar <102718141+abhilashkar2001@users.noreply.github.com>
2025-09-25 00:37:35 +0530

    fix: env for sit

--- src/environments/environment.sit.ts
+++ src/environments/environment.sit.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  microServiceURL: `${window.location.origin}/api`,
-  parentAppUrl: `https://${window.location.host}:4200`,
+  microServiceURL: `https://170.10.1.85:8765`,
+  parentAppUrl: `https://10.137.161.200:2502/maintenance`,
   build: 33,
   SECRET_KEY:
     'e1cd356f6de53babfef423718f055d8034d5d580c4196f6c5f230e2ee395133b',
