2a39c74894ca261ef938bbb63e73c8cb8fd54fe6
Abhilash Kar <102718141+abhilashkar2001@users.noreply.github.com>
2025-09-24 12:19:15 +0530

    fix: errors

---
+++ src/environments/environment.sit.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  microServiceURL: `${window.location.origin}/api`,
+  parentAppUrl: `https://${window.location.host}:4200`,
+  build: 33,
+  SECRET_KEY:
+    'e1cd356f6de53babfef423718f055d8034d5d580c4196f6c5f230e2ee395133b',
+};
